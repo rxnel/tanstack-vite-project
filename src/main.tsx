9c22e3dc9d1794a11cc63ba554a4e203f5bb1c1a
ronel <ronel.aguilar@icloud.com>
2025-03-12 18:15:59 -0400

    testing/using usequery with supabase

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 
@@ -21,7 +21,9 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={new QueryClient()}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </StrictMode>,
   );
 }
--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,9 +1,30 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { useQuery } from "@tanstack/react-query";
+import supabase from "../utils/supabase";
 
 export const Route = createFileRoute("/about")({
   component: About,
 });
 
 function About() {
-  return <div className="p-2">Hello from About!</div>;
+  const { data } = useQuery({
+    queryKey: ["about"],
+    queryFn: getTodos,
+  });
+
+  if (data) {
+    return <div className="p-2">{data[0]?.title ?? "no posts"}</div>;
+  }
 }
+
+const getTodos = async () => {
+  const { data, error } = await supabase
+    .from("Todos")
+    .select("*")
+    .order("created_at", { ascending: false });
+
+  if (error) {
+    throw error;
+  }
+  return data;
+};
